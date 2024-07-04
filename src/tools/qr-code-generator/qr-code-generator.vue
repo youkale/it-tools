bfa0c58bb458daf65a4796154d6df108b1176970
emmmm~ <tdycxs@gmail.com>
2024-07-04 16:56:21 +0800

    Update qr-code-generator.vue

--- src/tools/qr-code-generator/qr-code-generator.vue
+++ src/tools/qr-code-generator/qr-code-generator.vue
@@ -9,7 +9,7 @@ const errorCorrectionLevel = ref<QRCodeErrorCorrectionLevel>('medium');
 
 const errorCorrectionLevels = ['low', 'medium', 'quartile', 'high'];
 
-const text = ref('https://it-tools.tech');
+const text = ref('https://1024code.tools');
 const { qrcode } = useQRCode({
   text,
   color: {
